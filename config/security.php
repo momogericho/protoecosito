ebf3f6275f7527d4df3708d2a55994774e6c66ca
momogericho <mohamed2000223@gmail.com>
2025-09-14 15:41:40 +0200

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   .env.example 	modified:   README.MD 	modified:   config/security.php

--- config/security.php
+++ config/security.php
@@ -6,3 +6,11 @@ if (!defined('PEPPER')) {
     }
     define('PEPPER', $pepper);
 }
+
+if (!defined('REMEMBER_KEY')) {
+    $rememberKey = getenv('REMEMBER_KEY');
+    if ($rememberKey === false) {
+        throw new RuntimeException('REMEMBER_KEY environment variable not set');
+    }
+    define('REMEMBER_KEY', $rememberKey);
+}
